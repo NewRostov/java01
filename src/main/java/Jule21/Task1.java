33c5e2343f1f11f69561fcef0da2ca6346083716
NewRostov <kim.sergey.rostov@gmail.com>
2023-07-21 04:33:36 +0200

    Task4

---
+++ src/main/java/Jule21/Task1.java
@@ -0,0 +1,20 @@
+package Jule21;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Task1 {
+    public static void main(String[] args) {
+        TreeMap<Integer, String> n = new TreeMap<>();
+        n.put(111111, "Ivanov");
+        n.put(222222, "Ivanov");
+        n.put(333333, "Petrov");
+        n.put(444444, "Sidorov");
+        n.put(555555, "Ivanov");
+        n.put(777777, "Ivanov");
+        n.put(666666, "Ivanov");
+        System.out.println(n);
+        }
+
+
+}
